f702615fd295064e71a9359808b7c8ccca664551
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2025-06-14 23:21:28 +0500

    v0.2.1

--- mood-tracker/app/lib/mongodb.ts
+++ mood-tracker/app/lib/mongodb.ts
@@ -15,11 +15,10 @@ if (!cached) {
 
 async function dbConnect() {
   if (cached.conn) return cached.conn
-
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
       bufferCommands: false,
-    }).then((mongoose) => mongoose)
+    }).then((mongoose) => mongoose.connection)
   }
   cached.conn = await cached.promise
   return cached.conn
